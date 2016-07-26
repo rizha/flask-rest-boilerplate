79924e823bc06afb970f4d34d4c739422a184b78
rizha <rizha.musthafa@gmail.com>
2016-07-26 15:43:06 +0700

    add user detail and list

--- app/api/resources/users.py
+++ app/api/resources/users.py
@@ -1,6 +1,7 @@
-from flask.ext.restful import Resource, reqparse
+from flask.ext.restful import Resource, reqparse, marshal, fields
 
 from app.users import User
+from app import mongo
 
 
 class Auth(Resource):
@@ -18,3 +19,44 @@ class Auth(Resource):
             return user['data'], 401
 
         return user['data'], 200
+
+
+class UserList(Resource):
+
+    def post(self):
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', type=str, required=True, location='json')
+        parser.add_argument('username', type=str, required=True,
+                            location='json')
+        parser.add_argument('password', type=str, required=True,
+                            location='json')
+
+        data = parser.parse_args()
+        user = User.create_user(data['email'], data['username'],
+                                data['password'])
+
+        if not user['status']:
+            return user['data'], 409
+
+        return None, 201
+
+
+class UserDetail(Resource):
+
+    def get(self, username):
+        resource_field = {
+            'username': fields.String,
+            'email': fields.String,
+            'fullName': fields.String,
+            'phone': fields.String,
+            'gender': fields.String,
+            'dateUpdated': fields.DateTime(dt_format='iso8601'),
+            'dateCreated': fields.DateTime(dt_format='iso8601')
+        }
+
+        user = mongo.db.users.find_one({'username': username}, {'_id': 0})
+
+        output = marshal(user, resource_field)
+
+        return {'res': output}
