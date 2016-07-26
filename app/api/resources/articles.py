add711c39f5f7c1108ef0f401560ca7bb0512696
rizha <rizha.musthafa@gmail.com>
2016-07-26 15:59:50 +0700

    add article create resource

---
+++ app/api/resources/articles.py
@@ -0,0 +1,17 @@
+from flask.ext.restful import Resource, reqparse
+
+from app.blog import Article
+
+
+class ArticleList(Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', type=str, required=True, location='json')
+        parser.add_argument('description', type=str, required=True,
+                            location='json')
+
+        data = parser.parse_args()
+        user = Article.create(data['title'], data['description'])
+
+        return user['data'], 200
