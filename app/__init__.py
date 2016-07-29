e7cc8423eeadd7107e52e6579f554a8dfa5309db
rizha <rizha.musthafa@gmail.com>
2016-07-29 18:57:55 +0700

    change flask-cache to werkzeug RedisCache

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 import logging
 
+from werkzeug.contrib.cache import RedisCache
 from flask import Flask
-from extensions import mongo, cache, celery
+from extensions import mongo, celery
 from config import config
 
 
@@ -9,12 +10,13 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
-    from api import api_bp as api_blueprint
+    from .api import api_bp as api_blueprint
 
     app.register_blueprint(api_blueprint, url_prefix='/api')
 
+    app.cache = RedisCache(app.config['REDIS_HOST'])
+
     mongo.init_app(app)
-    cache.init_app(app)
     celery.conf.update(app.config)
 
     if app.debug is False:
